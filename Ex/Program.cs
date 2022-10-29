05064c4b100c5479c9b1d7ebd37ea02ef5062e6c
Vasilya <lady_drakula_94@mail.ru>
2022-10-29 20:46:13 +0500

    Ex Напила программу

---
+++ Ex/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+string[] array = { "Hello", "2", "world", ":-)" };
+int size = array.Length;
+string[] newArray = new string[new Random().Next(0, 3)];
+Console.WriteLine($"newArray.Length = {newArray.Length}");
+int j = 0;
+Console.WriteLine("newArray: ");
+for (int i = 0; i < newArray.Length; i++)
+{
+    j = new Random().Next(array.Length);
+    newArray[i] = array[j];
+    Console.Write(newArray[i]+" ");       
+}
+
+
